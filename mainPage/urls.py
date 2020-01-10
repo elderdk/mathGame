b51e630a720aad7796f6406c978f4bfd06c209ec
Ubuntu <ubuntu@ip-172-26-8-192.ap-northeast-2.compute.internal>
2020-01-10 03:56:41 +0000

    fix gitignore

--- mainPage/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.home, name = 'home'),
-    path('math/', views.math, name = 'mathPage' ),
-    path('authenticate/<str:username>/', views.auth_user, name ='authenticate'),
-    path('math/scoreup/', views.scoreup, name = "scoreup"),
-]
