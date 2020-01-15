6fd6c639dd33bbc52f1d6afded0a514953975567
Ubuntu <elder.dk@gmail.com>
2020-01-15 08:44:36 +0000

    Add chores

--- chores/static/chores/chores.js
+++ chores/static/chores/chores.js
@@ -1,3 +1,9 @@
 const choreBoxes = document.querySelectorAll('.choreBox')
+const successSound = document.querySelector('#successSound')
 
 // Add event listeners
+choreBoxes.forEach(e => {
+    e.addEventListener('click', event => {
+        successSound.play();
+    });
+})
