e970afc7c116e6d3fa16f9601dddfafee30e4970
DanielK <elder.dk@gmail.com>
2020-09-12 13:10:26 +0900

    add chore image

--- chores/models.py
+++ chores/models.py
@@ -1,16 +1,28 @@
 from django.db import models
 from django.contrib.auth.models import User
+from PIL import Image
 
 # Create your models here.
 class Chore(models.Model):
     name = models.CharField(max_length = 250)
     score = models.IntegerField()
-    duration = models.DurationField()
     description = models.CharField(max_length = 1000)
     user_assign = models.ManyToManyField(User)
+    image = models.ImageField(upload_to='chore_images', default='cat.png')
+    show_by_image = models.BooleanField(default=False)
 
     def __str__(self):
         return self.name
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+        
+        img = Image.open(self.image.path)
+
+        if img.height > 300 or img.width > 300:
+            output_size = (300, 300)
+            img.thumbnail(output_size)
+            img.save(self.image.path)
 
 class ChoreLog(models.Model):
     user_did = models.ForeignKey(User, on_delete = models.CASCADE, related_name = 'log_chore')
